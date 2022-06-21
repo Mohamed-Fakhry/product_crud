62f7cff7428a75923c0cc6d6c82c32472f02f238
Mohamed Fakhry <mohamedfakhry58890@gmail.com>
2022-06-21 19:52:15 +0200

    Add product page

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,7 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 
+import '../features/products_page.dart';
+
 @MaterialAutoRouter(
   replaceInRouteName: 'Page,Route',
-  routes: <AutoRoute>[],
+  routes: <AutoRoute>[
+    AutoRoute(page: ProductPage, path: '/login'),
+  ],
 )
 class $AppRouter {}
